b4b7b5bc48fec816f59308d4f18aac9ae2770767
Guilherme Silva <sgsouza.173@gmail.com>
2024-09-01 09:59:04 -0300

    Initial commit

---
+++ src/main/java/com/guizao/Chatbot_t1/ChatbotT1Application.java
@@ -0,0 +1,13 @@
+package com.guizao.Chatbot_t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/guizao/Chatbot_t1/ChatbotT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guizao.Chatbot_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
